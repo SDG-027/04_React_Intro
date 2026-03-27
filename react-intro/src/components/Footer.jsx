a8ed06733e3b8ee35e493c3156bda06710a59f7f
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-03-27 11:09:00 +0100

    React Intro

--- react-intro/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  // Komponenten berechnen ihre UI oft dynamisch
-  const date = new Date();
-  const year = date.getFullYear();
-
-  return <footer>&copy; {year}</footer>;
-}
-
-export default Footer;
